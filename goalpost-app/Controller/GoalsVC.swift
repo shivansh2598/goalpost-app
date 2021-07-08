c4a4d85aa67503a22fb8eb261286b1768d92ab58
shivansh <shivanshsinha2598@gmail.com>
2021-07-09 00:01:55 +0530

    delegate and datasource created

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -12,12 +12,29 @@ class GoalsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
         debugPrint("Button was pressed")
     }
     
+}
+
+extension GoalsVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week", type: "Short Term", goalProgressAmount: 2)
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
 }
 
--- goalpost-app/Views/GoalCell.swift
+++ goalpost-app/Views/GoalCell.swift
@@ -12,7 +12,7 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
     
-    func configureCell(description: String, type: String, goalProgressAmount: String){
+    func configureCell(description: String, type: String, goalProgressAmount: Int){
         self.goalDescritionLbl.text = description
         self.goalTypeLbl.text = type
         self.goalProgressLbl.text = String(describing: goalProgressLbl)
