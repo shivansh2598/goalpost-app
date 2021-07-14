6c4480888ce0ecd9e8c2db0432703037a357aa43
shivansh <shivanshsinha2598@gmail.com>
2021-07-14 16:43:47 +0530

    finished view added, project done

--- goalpost-app/Views/GoalCell.swift
+++ goalpost-app/Views/GoalCell.swift
@@ -11,11 +11,18 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescritionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var finishTaskDisplay : UIView!
     
     func configureCell(goal : Goal){
         self.goalDescritionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType
         self.goalProgressLbl.text = String(describing: goal.goalProgress)
+        
+        if goal.goalProgress == goal.goalCompletionValue {
+            finishTaskDisplay.isHidden = false
+        } else {
+            finishTaskDisplay.isHidden = true
+        }
     }
 }
 
